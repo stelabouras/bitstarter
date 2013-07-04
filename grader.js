7eaacd3c6488848553fdcb9924e2aff1b2d20148
Stelios Petrakis <stelabouras@gmail.com>
2013-07-04 08:05:39 +0000

    Changed grader.js to check urls with restler

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -44,6 +45,21 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
+var checkURL = function(url, checksfile) {
+	rest.get(url).on('complete', function(data) {
+		$ = cheerio.load(data);
+		var checks = loadChecks(checksfile).sort();
+		var out = {};
+		for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+			out[checks[ii]] = present;
+		}
+
+    		var outJson = JSON.stringify(out, null, 4);
+    		console.log(outJson);
+	});
+};
+
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
@@ -59,10 +75,17 @@ if(require.main == module) {
     program
         .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
         .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+	.option('-u, --url [url] ', 'Site url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+	if(typeof program.url != 'undefined')
+		checkURL(program.url, program.checks);
+	else {
+  		var checkJson = checkHtmlFile(program.file, program.checks);
+    		var outJson = JSON.stringify(checkJson, null, 4);
+    		console.log(outJson);
+	}
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
